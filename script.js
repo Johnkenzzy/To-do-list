e8ba6416857e30f1bd9f7148b3941e0414fd54ed
Johnkenzzy <johnkenumeh@gmail.com>
2023-07-10 09:53:08 +0100

    Updated the to-do list

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 
 
 const input = document.querySelector("input");
-const btn = document.querySelector("#btn");
-const outputCon = document.querySelector(".output-container");
+const btn = document.querySelector("button");
+const output = document.querySelector(".output-container");
 
 
 btn.addEventListener("click", function(){
-    let ul = document.createElement("ul");
-    outputCon.appendChild(ul)
+    
+    let task = document.createElement("ul");
+    task.classList.add("task");
 
     let li = document.createElement("li");
     li.innerText = `${input.value}`;
-    ul.appendChild(li);
+    task.appendChild(li);
 
-    if (input.value === ""){
+    const chkBtn = document.createElement("i");
+    chkBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    task.appendChild(chkBtn);
+    chkBtn.addEventListener("click", function(){
+        li.style.textDecoration = "line-through";
+        li.style.textDecorationColor = "red";
+        li.style.textDecorationThickness = "3px";
+    });
+    
+    let delBtn = document.createElement("i");
+    delBtn.innerHTML = `</i><i class="fa-solid fa-trash"></i>`;
+    task.appendChild(delBtn);
+    delBtn.addEventListener("click", function(){
+        task.style.display = "none";
+    });
+    
+
+    if(input.value === ""){
         alert("Please enter a valid task");
-    }else {
-        ul.appendChild(li);
+    }else{
+        output.appendChild(task);
     }
-
+     input.value = "";
 });
+
+
+
